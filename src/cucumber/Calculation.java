2ef096c02e509b94ea486a87e3fe0485e9f8b912
khushi2213 <kkhushee.khushi@gmail.com>
2023-05-11 17:24:03 +0100

    practice work

---
+++ src/cucumber/Calculation.java
@@ -0,0 +1,12 @@
+package cucumber;
+
+public class Calculation
+{
+     public static void main(String[] args)
+     {
+         float result=63*5/2f;
+         System.out.println(result);
+
+     }
+
+}
---
+++ src/cucumber/JustPrint.java
@@ -0,0 +1,12 @@
+package cucumber;
+
+public class JustPrint
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Khushbu");
+        System.out.println(" Pandey");
+
+    }
+}
+
